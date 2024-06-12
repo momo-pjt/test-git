29eb801b3a6117a23e9a4409e1083acaba4a0206
yjin <yyyyyjindev@gmail.com>
2024-06-12 17:13:53 +0900

    asd

---
+++ src/main/java/me/yjin/badaga/BadagaApplication.java
@@ -0,0 +1,13 @@
+package me.yjin.badaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadagaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BadagaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/yjin/badaga/BadagaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yjin.badaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadagaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
