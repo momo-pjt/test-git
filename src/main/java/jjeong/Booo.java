af95ba77850be06e1278c7cf5d8f83053775b791
yoojeong <frozen58@naver.com>
2024-06-12 17:47:51 +0900

    youuujeong

--- src/main/java/jjeong/Booo.java
+++ src/main/java/jjeong/Booo.java
@@ -3,5 +3,6 @@ package jjeong;
 public class Booo {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello2");
     }
 }
--- src/main/java/jjeong/Meeuuu.java
+++
@@ -1,9 +0,0 @@
-package jjeong;
-
-
-public class Meeuuu {
-    public static void main(String[] args) {
-        System.out.println("하이");
-        System.out.println("하이2");
-    }
-}
